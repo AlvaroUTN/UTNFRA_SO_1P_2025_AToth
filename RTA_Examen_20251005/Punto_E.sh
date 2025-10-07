69eb96a7f0ff78c1546de849e1ea2e613242e72f
Alvaro Toth <alvatoth@gmail.com>
2025-10-07 23:02:52 +0000

    primeros pasas punt E

--- RTA_Examen_20251005/Punto_E.sh
+++ RTA_Examen_20251005/Punto_E.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-Hola
+sudo chown -R p1c1_2025_u1:p1c1_2025_gTodos /Punto_D/Libros
+sudo chown -R p1c1_2025_u2:p1c1_2025_gTodos /Punto_D/Musica
+sudo chown -R p1c1_2025_u3:p1c1_2025_gTodos /Punto_D/Videos
+
+
+sudo chmod 750 /Punto_D/Libros
+sudo chmod 715 /Punto_D/Musica
+sudo chmod 774 /Punto_D/Videos
