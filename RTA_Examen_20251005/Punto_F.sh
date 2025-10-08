67a025dab47b27648b0ef77df1c24d015530345f
Alvaro Toth <alvatoth@gmail.com>
2025-10-08 21:59:22 +0000

    Terminacion del punto F

--- RTA_Examen_20251005/Punto_F.sh
+++ RTA_Examen_20251005/Punto_F.sh
@@ -3,4 +3,10 @@
 
 mkdir -p ~/Punto_F
 
+grep "MemTotal" /proc/meminfo | awk '{print $2}' | tee ~/Punto_F/Filtro_basico.txt
+
+grep "model name" /proc/cpuinfo | head -n 1 | awk -F': ' '{print $2}' | tee -a ~/Punto_F/Filtro_basico.txt
+
+grep "cpu MHz" /proc/cpuinfo | head -n 1 | awk '{print $4}' | tee -a ~/Punto_F/Filtro_basico.txt
+
 
